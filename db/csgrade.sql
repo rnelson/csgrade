bd861a36f896759af365ff06cab2e2ad76fada8e
Ross Nelson <ross.nelson@gmail.com>
2010-02-10 10:47:20 -0600

    Fixed errors in the SQL script, escaped all names, organized it a bit

--- db/csgrade.sql
+++ db/csgrade.sql
@@ -1,127 +1,130 @@
 # Creates the database for csgrade; assumes database does not exist
 # Version 1
 
-CREATE DATABASE csgrade;
-USE csgrade;
+DROP DATABASE IF EXISTS `csgrade`;
+CREATE DATABASE `csgrade`;
+USE `csgrade`;
 
-CREATE TABLE config (
-	version INTEGER NOT NULL DEFAULT 1,
-	adminName VARCHAR(255) NOT NULL,
-	adminEmail VARCHAR(255),
-	adminUrl VARCHAR(255),
-	defaultFilePath VARCHAR(255),
+CREATE TABLE `config` (
+	`dbVersion`			INTEGER NOT NULL DEFAULT 1,
+	`adminName`			VARCHAR(255) NOT NULL,
+	`adminEmail`		VARCHAR(255),
+	`adminUrl`			VARCHAR(255),
+	`defaultFilePath`	VARCHAR(255),
+	
+	PRIMARY KEY (`dbVersion`)
 );
 
-CREATE TABLE user (
-	id INTEGER NOT NULL AUTO_INCREMENT,
-	userTypeId INTEGER NOT NULL, # -> userType(id)
-	username VARCHAR(255) NOT NULL,
-	passwd VARCHAR(255) NOT NULL, # password hash
-	firstName VARCHAR(255),
-	lastName VARCHAR(255),
-	email VARCHAR(255),
+CREATE TABLE `assignment` (
+	`id`				INTEGER NOT NULL AUTO_INCREMENT,
+	`name`				VARCHAR(255) NOT NULL,
+	`totalPoints`		INTEGER NOT NULL,
+	`weight`			DECIMAL(7, 4) NOT NULL,
+	`classAverage`		DECIMAL(7, 4) NOT NULL,
+	`classId`			INTEGER NOT NULL, # -> singleClass(id)
 	
-	PRIMARY KEY (id)
+	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE userType (
-	id INTEGER NOT NULL AUTO_INCREMENT,
-	name VARCHAR(255) NOT NULL,
-	privs INTEGER NOT NULL,
+CREATE TABLE `assignmentPart` (
+	`id`				INTEGER NOT NULL AUTO_INCREMENT,
+	`name`				VARCHAR(255) NOT NULL,
+	`totalPoints`		INTEGER NOT NULL,
+	`weight`			DECIMAL(7, 4) NOT NULL,
+	`classAverage`		DECIMAL(7, 4) NOT NULL,
+	`assignmentId`		INTEGER NOT NULL, # -> assignment(id)
+	`filePath`			VARCHAR(255),
 	
-	PRIMARY KEY (id)
+	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE priv (
-	id INTEGER NOT NULL AUTO_INCREMENT,
-	name VARCHAR(255) NOT NULL,
-	bitvalue INTEGER NOT NULL,
+CREATE TABLE `comment` (
+	`id`				INTEGER NOT NULL AUTO_INCREMENT,
+	`gradeId`			INTEGER DEFAULT 0, # -> grade(id)
+	`assignmentPartId`	INTEGER DEFAULT 0, # -> assignmentPart(id)
+	`userId`			INTEGER NOT NULL, # -> user(id)
+	`replyId`			INTEGER, # -> comment(id)
+	`commentText`		TEXT NOT NULL,
 	
-	PRIMARY KEY (id)
+	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE semester (
-	id INTEGER NOT NULL AUTO_INCREMENT,
-	name VARCHAR(255) NOT NULL,
-	startDate DATE NOT NULL,
-	endData DATE NOT NULL,
+CREATE TABLE `grade` (
+	`id`				INTEGER NOT NULL AUTO_INCREMENT,
+	`assignmentPartId`	INTEGER NOT NULL, # -> assignmentPart(id)
+	`studentId`			INTEGER NOT NULL, # -> user(id)
+	`points`			INTEGER NOT NULL,
+	`percentage`		DECIMAL(7, 4) NOT NULL,
+	`commentId`			INTEGER,
 	
-	PRIMARY KEY (id)
+	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE singleClass (
-	id INTEGER NOT NULL AUTO_INCREMENT,
-	name VARCHAR(255) NOT NULL,
-	semesterId INTEGER NOT NULL, # -> semester(id)
+CREATE TABLE `instructorType` (
+	`id`				INTEGER NOT NULL AUTO_INCREMENT,
+	`name`				VARCHAR(255),
 	
-	PRIMARY KEY (id)
+	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE studentClassXref (
-	id INTEGER NOT NULL AUTO_INCREMENT,
-	studentId INTEGER NOT NULL, # -> user(id)
-	classId INTEGER NOT NULL, # -> singleClass(id)
+CREATE TABLE `instructorClassXref` (
+	`id`				INTEGER NOT NULL AUTO_INCREMENT,
+	`instructorId`		INTEGER NOT NULL, # -> user(id)
+	`classId`			INTEGER NOT NULL, # -> singleClass(id)
+	`instructorTypeId`	INTEGER NOT NULL, # -> instructorType(id)
 	
-	PRIMARY KEY (id)
+	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE instructorType (
-	id INTEGER NOT NULL AUTO_INCREMENT,
-	name VARCHAR(255),
+CREATE TABLE `priv` (
+	`id`				INTEGER NOT NULL AUTO_INCREMENT,
+	`name`				VARCHAR(255) NOT NULL,
+	`bitvalue`			INTEGER NOT NULL,
 	
-	PRIMARY KEY (id)
+	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE instructorClassXref (
-	id INTEGER NOT NULL AUTO_INCREMENT,
-	instructorId INTEGER NOT NULL, # -> user(id)
-	classId INTEGER NOT NULL, # -> singleClass(id)
-	instructorTypeId INTEGER NOT NULL, # -> instructorType(id)
+CREATE TABLE `semester` (
+	`id`				INTEGER NOT NULL AUTO_INCREMENT,
+	`name`				VARCHAR(255) NOT NULL,
+	`startDate`			DATE NOT NULL,
+	`endData`			DATE NOT NULL,
 	
-	PRIMARY KEY (id)
+	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE assignment (
-	id INTEGER NOT NULL AUTO_INCREMENT,
-	name VARCHAR(255) NOT NULL,
-	totalPoints INTEGER NOT NULL,
-	weight DECIMAL(3, 4) NOT NULL,
-	classAverage DECIMAL(4, 4) NOT NULL,
-	classId INTEGER NOT NULL, # -> singleClass(id)
+CREATE TABLE `singleClass` (
+	`id`				INTEGER NOT NULL AUTO_INCREMENT,
+	`name`				VARCHAR(255) NOT NULL,
+	`semesterId`		INTEGER NOT NULL, # -> semester(id)
 	
-	PRIMARY KEY (id)
+	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE assignmentPart (
-	id INTEGER NOT NULL AUTO_INCREMENT,
-	name VARCHAR(255) NOT NULL,
-	totalPoints INTEGER NOT NULL,
-	weight DECIMAL(3, 4) NOT NULL,
-	classAverage DECIMAL(4, 4) NOT NULL,
-	assignmentId INTEGER NOT NULL, # -> assignment(id)
-	filePath VARCHAR(255),
+CREATE TABLE `studentClassXref` (
+	`id`				INTEGER NOT NULL AUTO_INCREMENT,
+	`studentId`			INTEGER NOT NULL, # -> user(id)
+	`classId`			INTEGER NOT NULL, # -> singleClass(id)
 	
-	PRIMARY KEY (id)
+	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE grade (
-	id INTEGER NOT NULL AUTO_INCREMENT,
-	assignmentPartId INTEGER NOT NULL, # -> assignmentPart(id)
-	studentId INTEGER NOT NULL, # -> user(id)
-	points INTEGER NOT NULL,
-	percentage DECIMAL(4, 4) NOT NULL,
-	commentId INTEGER,
+CREATE TABLE `user` (
+	`id`				INTEGER NOT NULL AUTO_INCREMENT,
+	`userTypeId`		INTEGER NOT NULL, # -> userType(id)
+	`username`			VARCHAR(255) NOT NULL,
+	`passwd`			VARCHAR(255) NOT NULL, # password hash
+	`firstName`			VARCHAR(255),
+	`lastName`			VARCHAR(255),
+	`email`				VARCHAR(255),
 	
-	PRIMARY KEY (id)
+	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE comment (
-	id INTEGER NOT NULL AUTO_INCREMENT,
-	gradeId INTEGER DEFAULT 0, # -> grade(id)
-	assignmentPartId INTEGER DEFAULT 0, # -> assignmentPart(id)
-	userId INTEGER NOT NULL, # -> user(id)
-	replyId INTEGER, # -> comment(id)
-	commentText TEXT NOT NULL,
+CREATE TABLE `userType` (
+	`id`				INTEGER NOT NULL AUTO_INCREMENT,
+	`name`				VARCHAR(255) NOT NULL,
+	`privs`				INTEGER NOT NULL,
 	
-	PRIMARY KEY (id)
+	PRIMARY KEY (`id`)
 );
